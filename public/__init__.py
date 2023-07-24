15ec9683a661e9446a8e3651615e8dd232e8bcb0
Jonathan Bangert <jonathan@bangert.dk>
2023-07-24 17:37:55 +0200

    Initial commit

---
+++ public/__init__.py
@@ -0,0 +1,7 @@
+"""Frontend for Music Assistant."""
+from pathlib import Path
+
+
+def where() -> Path:
+    """Return path to the frontend."""
+    return Path(__file__).parent
---
+++ vite.config.ts
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import { VitePWA } from 'vite-plugin-pwa';
+import { fileURLToPath, URL } from 'node:url';
+import webfontDownload from 'vite-plugin-webfont-dl';
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  base: './',
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    webfontDownload(['https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap']),
+    vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    }),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Music Assistant',
+        short_name: 'Music library manager',
+        description:
+          'Music Assistant is a free, opensource Media library manager that connects to your streaming services and a wide range of connected speakers.',
+        theme_color: '#424242',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+    VueI18nPlugin({
+      include: [path.resolve(__dirname, './src/translations/**')],
+    })
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
+  },
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
+  // tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+}));
