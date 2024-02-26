b97c77ca5090db6a83af9d6e17c7cb70ec705bd3
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2024-02-26 13:12:10 +0000

    Fix invalid string conversion

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -67,8 +67,7 @@ fn start_sqzlite(ip: String, output_device: String, port: String) {
                     "Companion",
                     "-n",
                     combined_hostname
-                        .to_str()
-                        .expect("Couldnt convert hostname to &str -_-"),
+                        .as_str(),
                     "-o",
                     output_device.as_str(),
                 ])
